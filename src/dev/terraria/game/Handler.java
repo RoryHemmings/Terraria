486e041e6bf0bd944df6a6bdaae53b167a84ef7d
RoryHemmings <rpmhemmings@gmail.com>
2017-09-27 15:23:32 -0700

    added clamp method
    added clamp method to handler class

--- src/dev/terraria/game/Handler.java
+++ src/dev/terraria/game/Handler.java
@@ -30,4 +30,8 @@ public class Handler {
 	public World getWorld() {
 		return world;
 	}
+	
+	public static float clamp(float val, float min, float max) {
+	    return Math.max(min, Math.min(max, val));
+	}
 }
--- src/dev/terraria/game/tiles/StoneTile.java
+++ src/dev/terraria/game/tiles/StoneTile.java
@@ -2,7 +2,7 @@ package dev.terraria.game.tiles;
 
 import dev.terraria.game.gfx.Assets;
 
-public class StoneTile extends Tile{
+public class StoneTile extends Tile {
 	public StoneTile(int id) {
 		super(Assets.stone_block, id);
 	}
